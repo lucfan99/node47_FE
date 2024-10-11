27ac978333674dd1d6c7196268248b84a092efb1
lucfan99 <tranxuanluc9@gmail.com>
2024-10-11 19:39:15 +0700

    code buoi 07

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,6 +3,7 @@ import { Box, Stack, Typography } from "@mui/material";
 
 import { Videos, Sidebar } from "./";
 import { useParams } from "react-router-dom";
+import { getVideosAPI, getVideosTypeIdAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
@@ -11,31 +12,82 @@ const Feed = () => {
   const params = useParams();
 
   useEffect(() => {
-
-
-
+    getVideosTypeIdAPI(params.id)
+      .then((result) => {
+        setVideos(result);
+      })
+      .catch(() => {
+        setVideos([]);
+        console.log("error call api get list video form type_id");
+      });
   }, [params.id]);
 
   useEffect(() => {
-    let lstItem = [{ video_id: 1, video_name: "Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI", channelDetail: "", marginTop: " ", thumbnail: "https://i.ytimg.com/vi/QU9c0053UAU/hq720.jpg", channelId: 1, channelTitle: "abc", channelId: 1, channelTitle: "JavaScript Mastery" },
-    { video_id: 2, video_name: "The movies Iron man 4: 0.1 Hours", channelDetail: "", marginTop: " ", thumbnail: "https://i.ytimg.com/vi/t86sKsR4pnk/hq720.jpg", channelId: 1, channelTitle: "abc", channelId: 1, channelTitle: "JavaScript Mastery" }
-    ];
-
-    setVideos(lstItem)
-  }, [])
+    // let lstItem = [
+    //   {
+    //     video_id: 1,
+    //     video_name:
+    //       "Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI",
+    //     channelDetail: "",
+    //     marginTop: " ",
+    //     thumbnail: "https://i.ytimg.com/vi/QU9c0053UAU/hq720.jpg",
+    //     channelId: 1,
+    //     channelTitle: "abc",
+    //     channelId: 1,
+    //     channelTitle: "JavaScript Mastery",
+    //   },
+    //   {
+    //     video_id: 2,
+    //     video_name: "The movies Iron man 4: 0.1 Hours",
+    //     channelDetail: "",
+    //     marginTop: " ",
+    //     thumbnail: "https://i.ytimg.com/vi/t86sKsR4pnk/hq720.jpg",
+    //     channelId: 1,
+    //     channelTitle: "abc",
+    //     channelId: 1,
+    //     channelTitle: "JavaScript Mastery",
+    //   },
+    // ];
+    getVideosAPI()
+      .then((result) => {
+        console.log("get result:", result);
+        setVideos(result);
+      }) //khi success
+      .catch(() => {
+        console.log("error call api from BE");
+      }); // khi fail
+  }, []);
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           Copyright © 2050 Media
         </Typography>
       </Box>
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
           {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
 
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,23 +1,59 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = "http://localhost:8080";
 
 const options = {
   params: {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
     // 'token': localStorage.getItem("LOGIN_USER")
   },
 };
-
-
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
   return data;
 };
+// define function call api get list video tu BE
+export const getVideosAPI = async () => {
+  try {
+    const result = await axios.get(`${BASE_URL}/video/get-videos`);
+    console.log(result);
+    return result.data;
+  } catch (err) {
+    console.log("error api get list video");
+  }
+};
 
+//define function call api get list type video
+export const getTypeVideoAPI = async () => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/video/get-types`);
+    return data;
+  } catch (err) {
+    console.log("error api get list type");
+  }
+};
+
+//define function call api get list video by type_id tu BE
+export const getVideosTypeIdAPI = async (typeId) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/video/get-videos/${typeId}`);
+    return data;
+  } catch (err) {
+    console.log("error api get list video by type_id");
+  }
+};
+
+export const getVideoById = async (videoId) => {
+  try {
+    const { data } = await axios.get(`${BASE_URL}/video/get-video/${videoId}`);
+    return data;
+  } catch (err) {
+    console.log("err api get video by id");
+  }
+};
